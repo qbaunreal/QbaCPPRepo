9c2da555ba227ddd50a8eb8f9113261330de95ec
qbaunreal <122815600+qbaunreal@users.noreply.github.com>
2023-02-17 18:46:50 +0100

    Initial commit after moving the project to another repo

---
+++ CPPSolution/CPPSolution.cpp
@@ -0,0 +1,16 @@
+// CPPSolution.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "CPPSolution.h"
+#include "Examples1.h"
+
+
+int main()
+{
+	//ConstructSectionFromClass<Section6>();
+	/*ConstructSectionFromClass<Section7>();*/
+	ConstructSectionFromClass<Section8>();
+	return 0;
+}
+
+
